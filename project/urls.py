5a22eae23cbba1d794cde8704a1090eddb5a6571
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-07-26 15:48:42 -0400

    Adding routes to Recipes

--- project/urls.py
+++ project/urls.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('HOME')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('recipes/', include('recipes.urls')),
 ]
 
